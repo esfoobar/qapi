a1eeacc2ef4c70bc5df04ec0a8c72be3bffb9294
Jorge Escobar <j@jorge3.com>
2021-09-05 01:24:04 +0000

    created tables

--- app/models.py
+++ app/models.py
@@ -7,13 +7,14 @@ app_table = Table(
     "app",
     metadata,
     Column("id", Integer, primary_key=True),
-    Column("name", Integer, primary_key=True),
+    Column("name", String(80)),
     Column("secret", String(80)),
 )
 
 app_access_table = Table(
     "app_access",
     metadata,
+    Column("id", Integer, primary_key=True),
     Column("app_id", Integer, ForeignKey("app.id")),
     Column("token", String(35)),
     Column("expires", DateTime(timezone=True)),
