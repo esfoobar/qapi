22b94d552ad515b9a37688053023e0fecc78e90e
Jorge Escobar <j@jorge3.com>
2021-09-05 01:19:04 +0000

    first alembic run

--- app/api.py
+++ app/api.py
@@ -41,11 +41,6 @@ class AppAPI(MethodView):
 
 
 class AccessAPI(MethodView):
-    def __init__(self):
-        if not request.json:
-            abort(400)
-
-
 #     def post(self):
 #         if not "app_id" in request.json or not "app_secret" in request.json:
 #             error = {"code": "MISSING_APP_ID_OR_APP_SECRET"}
